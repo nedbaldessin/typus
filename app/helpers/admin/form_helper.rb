nedbaldessin/typus

commit 88787d2cf2dbacac9bfacf5518cf6be0f624503f

Removed the typus_template AR instance method because we want consistance in the UI.

app/helpers/admin/form_helper.rb
@@ -12,10 +12,7 @@ module Admin::FormHelper
       html << '<ul>'
 
       fields.each do |key, value|
-        if template = @resource[:class].typus_template(key)
-          html << typus_template_field(key, template, options)
-          next
-        end
+
         html << case value
                 when :belongs_to  then typus_belongs_to_field(key)
                 when :selector    then typus_selector_field(key)
@@ -277,7 +274,7 @@ module Admin::FormHelper
   end
 
   def typus_template_field(attribute, template, options = {})
-    template_name = File.join(Typus::Configuration.options[:templates_folder], template)
+    template_name = File.join('admin', 'templates', template)
     render :partial => template_name, 
            :locals => { :resource => @resource, :attribute => attribute, :options => options }
   end

lib/typus/active_record.rb
@@ -204,14 +204,6 @@ module Typus
       return date_format
     end
 
-    # We are able to define which template to use to render the attribute 
-    # within the form
-    def typus_template(attribute)
-      Typus::Configuration.config[name]['fields']['options']['templates'][attribute.to_s]
-    rescue
-      nil
-    end
-
     def build_conditions(params)
 
       conditions, joins = merge_conditions, []

generators/typus/templates/config/initializers/typus.rb
@@ -10,7 +10,6 @@ Typus::Configuration.options[:app_name] = '<%= application %>'
 # Typus::Configuration.options[:relationship] = 'typus_users'
 # Typus::Configuration.options[:root] = 'admin'
 # Typus::Configuration.options[:ssl] = false
-# Typus::Configuration.options[:templates_folder] = 'admin/templates'
 # Typus::Configuration.options[:user_class_name] = 'TypusUser'
 # Typus::Configuration.options[:user_fk] = 'typus_user_id'
 

lib/typus/configuration.rb
@@ -12,7 +12,6 @@ module Typus
                       :relationship => 'typus_users', 
                       :root => 'admin', 
                       :ssl => false, 
-                      :templates_folder => 'admin/templates',
                       :user_class_name => 'TypusUser', 
                       :user_fk => 'typus_user_id' }
 

test/lib/active_record_test.rb
@@ -369,16 +369,6 @@ class ActiveRecordTest < ActiveSupport::TestCase
 
   end
 
-  def test_should_verify_typus_template_is_working_properly
-
-   assert_equal 'datepicker', Post.typus_template('published_at')
-   assert_equal 'datepicker', Post.typus_template(:published_at)
-
-   assert_nil Post.typus_template('created_at')
-   assert_nil Post.typus_template('unknown')
-
-  end
-
   def test_should_verify_typus_user_id
     assert Post.typus_user_id?
     assert !TypusUser.typus_user_id?

test/lib/configuration_test.rb
@@ -17,7 +17,6 @@ class ConfigurationTest < ActiveSupport::TestCase
     assert_equal 'typus_users', Typus::Configuration.options[:relationship]
     assert_equal 'admin', Typus::Configuration.options[:root]
     assert_equal false, Typus::Configuration.options[:ssl]
-    assert_equal 'admin/templates', Typus::Configuration.options[:templates_folder]
     assert_equal 'TypusUser', Typus::Configuration.options[:user_class_name]
     assert_equal 'typus_user_id', Typus::Configuration.options[:user_fk]
   end

commit 06af9c8dcbcef47e051a8548efde6c85eea4057e

Fixing regression appeared in commit 88787d2cf2dbacac9bfacf5518cf6be0f624503f that broke custom attribute templates

app/helpers/admin/form_helper.rb
@@ -12,7 +12,10 @@ module Admin::FormHelper
       html << '<ul>'
 
       fields.each do |key, value|
-
+        if template = @resource[:class].typus_template(key)
+          html << typus_template_field(key, template, options)
+          next
+        end
         html << case value
                 when :belongs_to  then typus_belongs_to_field(key)
                 when :tree        then typus_tree_field(key)

lib/typus/active_record.rb
@@ -205,6 +205,14 @@ module Typus
       date_format = Typus::Configuration.config[name]['fields']['options']['date_formats'][attribute.to_s].to_sym rescue nil
       return !date_format.nil? ? date_format : :db
     end
+    
+    # We are able to define which template to use to render the attribute 
+    # within the form
+    def typus_template(attribute)
+      Typus::Configuration.config[name]['fields']['options']['templates'][attribute.to_s]
+    rescue
+      nil
+    end
 
     def build_conditions(params)
 